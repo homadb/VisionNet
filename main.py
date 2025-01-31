73c49b68f485d635aaa494f10ab6191753f8a8ae
Ali Divband <alidivbnd@gmail.com>
2025-01-31 19:47:11 +0100

    add images to test the model

--- main.py
+++ main.py
@@ -34,6 +34,8 @@ model = models.load_model('image_classifier.keras')
 
 
 
+
+
 # # building a CNN 
 # model = models.Sequential()
 # model.add(layers.Conv2D(32, (3,3), activation='relu', input_shape=(32,32,3)))
